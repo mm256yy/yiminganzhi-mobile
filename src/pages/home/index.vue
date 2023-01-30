12db0e8ad7b754ab67d32d3805e47d6a71826dc1
zhangmh@erongdu.com <zhangmh@erongdu.com>
2023-01-30 11:04:17 +0800

    feat: save

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -2,6 +2,7 @@
   <view>
     移民首页
     <view @click="jump">跳转到登录页面</view>
+    <view class="test-box">测试</view>
     <Test />
   </view>
 </template>
@@ -14,3 +15,11 @@ const jump = () => {
   routerForward('login')
 }
 </script>
+
+<style scoped lang="scss">
+.test-box {
+  width: 750rpx;
+  height: 200rpx;
+  background-color: green;
+}
+</style>
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,3 +1,39 @@
 <template>
-  <div>登陆页面</div>
+  <view class="login-wrap">
+    <view class="logo">
+      <image webp src="" class="img" />
+    </view>
+    <view class="form">
+      <view class="title">移民安置综合管理服务平台</view>
+      <view>账号密码登录</view>
+      <input type="text" placeholder="请输入账号" />
+      <input type="text" placeholder="请输入密码" />
+      <label> <checkbox value="cb" checked="true" />自动登录 </label>
+      <text>忘记密码</text>
+      <button>登录</button>
+    </view>
+  </view>
 </template>
+
+<style lang="scss" scoped>
+.login-wrap {
+  display: flex;
+  height: 100vh;
+  justify-content: space-between;
+  align-items: stretch;
+
+  .logo {
+    width: 434rpx;
+    background-color: #295ee6;
+
+    .img {
+      width: 300rpx;
+      height: 300rpx;
+    }
+  }
+
+  .form {
+    background-color: #fff;
+  }
+}
+</style>
