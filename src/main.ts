213e618c48dd33e868a96ada1226751334e6ab4a
zhangmh@erongdu.com <zhangmh@erongdu.com>
2023-02-17 10:00:54 +0800

    feat: save

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { createSSRApp } from 'vue'
 import App from './App.vue'
-// import JsonViewer from 'vue-json-viewer-next'
+import './static/styles/common.scss'
 
 export function createApp() {
   const app = createSSRApp(App)
-  // app.use(JsonViewer)
   return {
     app
   }
