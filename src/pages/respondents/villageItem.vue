ebf6743f67e819d476620abf2d741a27fd2b2308
zhangmh@erongdu.com <zhangmh@erongdu.com>
2023-02-24 17:05:17 +0800

    feat: save

---
+++ src/pages/respondents/villageItem.vue
@@ -0,0 +1,140 @@
+<template>
+  <view class="village-item">
+    <!-- <image class="bg" src="@/static/images/respondents_right.png" mode="scaleToFill" /> -->
+    <view class="head">
+      <view class="head-lt">
+        <image class="user-icon" src="@/static/images/respondents_tit.png" mode="scaleToFill" />
+        <view class="name">放假的时间啊了</view>
+      </view>
+      <view class="head-rt">
+        <view class="edit-box">
+          <image class="remove-icon" src="@/static/images/remove.png" mode="scaleToFill" />
+        </view>
+      </view>
+    </view>
+
+    <view class="cont">
+      <view class="cont-item">
+        <image class="icon" src="@/static/images/people_circle.png" mode="scaleToFill" />
+        <view class="label">行政村:</view>
+        <view class="value">房间打开手</view>
+      </view>
+      <view class="cont-item">
+        <image class="icon" src="@/static/images/people_circle.png" mode="scaleToFill" />
+        <view class="label">街道:</view>
+        <view class="value">房间打开手</view>
+      </view>
+      <view class="cont-item">
+        <image class="icon" src="@/static/images/people_circle.png" mode="scaleToFill" />
+        <view class="label">乡镇:</view>
+        <view class="value">房间打开手</view>
+      </view>
+      <view class="cont-item">
+        <image class="icon" src="@/static/images/people_circle.png" mode="scaleToFill" />
+        <view class="label">区县:</view>
+        <view class="value">房间打开手</view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<style lang="scss" scoped>
+.village-item {
+  position: relative;
+  width: 228px;
+  padding: 12rpx 15rpx;
+  margin: 0 7rpx 7rpx 0;
+  background-color: #ffffff;
+  border-radius: 7rpx;
+  box-shadow: 0rpx 0rpx 12rpx 0rpx rgba(0, 0, 0, 0.08);
+
+  &:nth-child(3n) {
+    margin-right: 0rpx;
+  }
+
+  .bg {
+    position: absolute;
+    top: -20rpx;
+    right: -20rpx;
+    width: 30rpx;
+    height: 30rpx;
+  }
+
+  .head {
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    padding-bottom: 6rpx;
+    justify-content: space-between;
+
+    .head-lt {
+      display: flex;
+      align-items: center;
+      flex-direction: row;
+
+      .user-icon {
+        width: 19rpx;
+        height: 19rpx;
+        margin-right: 6rpx;
+      }
+
+      .name {
+        margin-right: 6rpx;
+        font-size: 12rpx;
+        font-weight: 600;
+        color: #171718;
+      }
+    }
+
+    .head-rt {
+      display: flex;
+      align-items: center;
+      flex-direction: row;
+
+      .edit-box {
+        display: flex;
+        width: 16rpx;
+        height: 16rpx;
+        border: 1rpx solid #ff4242;
+        border-radius: 50%;
+        align-items: center;
+        justify-content: center;
+
+        .remove-icon {
+          width: 9rpx;
+          height: 9rpx;
+        }
+      }
+    }
+  }
+
+  .cont {
+    .cont-item {
+      display: flex;
+      height: 16rpx;
+      margin-top: 6rpx;
+      align-items: center;
+      flex-direction: row;
+
+      .icon {
+        width: 9rpx;
+        height: 9rpx;
+        margin-right: 6rpx;
+      }
+
+      .label {
+        width: 56rpx;
+        overflow: hidden;
+        font-size: 9rpx;
+        color: #171718;
+        word-break: keep-all;
+      }
+
+      .value {
+        font-size: 9rpx;
+        color: #171718;
+      }
+    }
+  }
+}
+</style>
