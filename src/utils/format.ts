2e3e2985a829e496651e6dd2dea65bbe9027125f
zhangwx <zhangwx@erongdu.com>
2023-02-28 16:20:48 +0800

    feat: 封装枚举类型解析function

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,8 +1,14 @@
 export const dictOption = (arr: any, val: any) => {
   let str = ''
   arr.map((item: any) => {
-    if (item.value.tostring() === val.tostring()) {
-      str = item.text
+    if (item.value === 'true') {
+      if (item.value === val.tostring()) {
+        str = item.text
+      }
+    } else {
+      if (item.value === val) {
+        str = item.text
+      }
     }
   })
   return str
