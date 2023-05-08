8d244752b41d2e28ec8ab4f89c9806b71ec79ae3
oldKing <zhangwx@erongdu.com>
2023-05-08 19:32:16 +0800

    feat: 坟墓信息所属村集体无法回显bug修改

--- src/pages/household/graveInfo/index.vue
+++ src/pages/household/graveInfo/index.vue
@@ -61,7 +61,7 @@
               <view class="col">
                 <view class="label">所属村集体：</view>
                 <view class="content">
-                  {{ dictOption(collectiveList, item.householdId) }}
+                  {{ dictOption(collectiveList, item.villageId) }}
                 </view>
               </view>
             </uni-col>
