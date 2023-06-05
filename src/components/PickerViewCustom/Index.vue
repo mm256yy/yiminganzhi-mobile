fd96aff6207e56234fda9bff556d4656a84a4736
oldKing <zhangwx@erongdu.com>
2023-06-05 19:45:14 +0800

    feat: 日期选择组件bug 修改

--- src/components/PickerViewCustom/Index.vue
+++ src/components/PickerViewCustom/Index.vue
@@ -3,13 +3,25 @@
     <slot>
       <view v-if="props.type === 'daterange'">
         <view class="range-date" v-if="currentValues && currentValues.length === 6">
-          <text class="txt">{{
-            currentValues[0] + '-' + (currentValues[1] + 1) + '-' + (currentValues[2] + 1)
-          }}</text>
+          <text class="txt">
+            {{
+              years()[currentValues[0]] +
+              '-' +
+              months()[currentValues[1]] +
+              '-' +
+              days(currentValues[0], currentValues[1])[currentValues[2]]
+            }}
+          </text>
           <text class="txt">&nbsp;-&nbsp;</text>
-          <text class="txt">{{
-            currentValues[3] + '-' + (currentValues[4] + 1) + '-' + (currentValues[5] + 1)
-          }}</text>
+          <text class="txt">
+            {{
+              years()[currentValues[3]] +
+              '-' +
+              months()[currentValues[4]] +
+              '-' +
+              days(currentValues[3], currentValues[4])[currentValues[5]]
+            }}
+          </text>
         </view>
         <view class="range-date" v-else>
           <text class="txt">开始时间</text>
@@ -19,9 +31,15 @@
       </view>
       <view v-else>
         <view class="range-date" v-if="currentValues && currentValues.length === 3">
-          <text class="txt">{{
-            currentValues[0] + '-' + (currentValues[1] + 1) + '-' + (currentValues[2] + 1)
-          }}</text>
+          <text class="txt">
+            {{
+              years()[currentValues[0]] +
+              '-' +
+              months()[currentValues[1]] +
+              '-' +
+              days(currentValues[0], currentValues[1])[currentValues[2]]
+            }}
+          </text>
         </view>
         <view class="range-date" v-else>
           <text class="txt">选择日期</text>
@@ -46,9 +64,9 @@
           <view class="item" v-for="(item, index) in months()" :key="index">{{ item }}</view>
         </picker-view-column>
         <picker-view-column>
-          <view class="item" v-for="(item, index) in days(startYear, startMonth)" :key="index">{{
-            item
-          }}</view>
+          <view class="item" v-for="(item, index) in days(startYear, startMonth + 1)" :key="index">
+            {{ item }}
+          </view>
         </picker-view-column>
         <picker-view-column v-if="props.type === 'daterange'">
           <view class="item" v-for="(item, index) in years()" :key="index">{{ item }}</view>
@@ -57,9 +75,9 @@
           <view class="item" v-for="(item, index) in months()" :key="index">{{ item }}</view>
         </picker-view-column>
         <picker-view-column v-if="props.type === 'daterange'">
-          <view class="item" v-for="(item, index) in days(endYear, endMonth)" :key="index">{{
-            item
-          }}</view>
+          <view class="item" v-for="(item, index) in days(endYear, endMonth + 1)" :key="index">
+            {{ item }}
+          </view>
         </picker-view-column>
       </picker-view>
     </view>
@@ -70,6 +88,7 @@
 import { ref, watch } from 'vue'
 import dayjs from 'dayjs'
 import { showToast } from '@/config/msg'
+import { getArrayIndex } from '@/utils'
 
 interface PropsType {
   type?: string
@@ -77,68 +96,10 @@ interface PropsType {
 }
 
 const props = defineProps<PropsType>()
-
 const visible = ref<boolean>(false)
-const startYear = ref<number>(dayjs().year())
-const startMonth = ref<number>(dayjs().month() + 1)
-const startDay = ref<number>(dayjs().date())
-const endYear = ref<number>(dayjs().year())
-const endMonth = ref<number>(dayjs().month() + 1)
-const endDay = ref<number>(dayjs().date())
 const indicatorStyle = `height: 50px;`
-const currentValues = ref<number[]>([])
 
 const emit = defineEmits(['open', 'confirm'])
-
-watch(
-  () => [props.type, props.value],
-  (val: any) => {
-    if (val && val.length) {
-      if (val[0] === 'date') {
-        let arr = val[1] && val[1].length ? val[1].split('-') : []
-        if (arr && arr.length) {
-          startYear.value = Number(arr[0])
-          startMonth.value = Number(arr[1]) - 1
-          startDay.value = Number(arr[2]) - 1
-        }
-        currentValues.value = [startYear.value, startMonth.value, startDay.value]
-      } else if (val[0] === 'daterange') {
-        let arr1 = val[1] && val[1].length ? val[1][0].split('-') : []
-        let arr2 = val[1] && val[1].length ? val[1][1].split('-') : []
-        if (arr1 && arr1.length) {
-          startYear.value = Number(arr1[0])
-          startMonth.value = Number(arr1[1]) - 1
-          startDay.value = Number(arr1[2]) - 1
-        }
-        if (arr2 && arr2.length) {
-          endYear.value = Number(arr2[0])
-          endMonth.value = Number(arr2[1]) - 1
-          endDay.value = Number(arr2[2]) - 1
-        }
-        currentValues.value = [
-          startYear.value,
-          startMonth.value,
-          startDay.value,
-          endYear.value,
-          endMonth.value,
-          endDay.value
-        ]
-      } else {
-        let arr = val[1] && val[1].length ? val[1].split('-') : []
-        if (arr && arr.length) {
-          startYear.value = Number(arr[0])
-          startMonth.value = Number(arr[1]) - 1
-          startDay.value = Number(arr[2]) - 1
-        }
-        currentValues.value = [startYear.value, startMonth.value, startDay.value]
-      }
-    }
-  },
-  {
-    immediate: true,
-    deep: true
-  }
-)
 
 const years = () => {
   let arr: any = []
@@ -165,6 +126,11 @@ const getDays = (year: number, month: number) => {
   return new Date(year, month, 0).getDate()
 }
 
+/**
+ * 获取每个月有多少天
+ * @param{Object} year 选择的年
+ * @param{Object} month 选择的月
+ */
 const days = (year: number, month: number) => {
   let arr: any = []
   for (let i = 1; i <= getDays(year, month); i++) {
@@ -173,23 +139,84 @@ const days = (year: number, month: number) => {
   return arr
 }
 
+const startYear = ref<number>(getArrayIndex(years(), dayjs().year()))
+const startMonth = ref<number>(getArrayIndex(months(), dayjs().month()))
+const startDay = ref<number>(
+  getArrayIndex(days(dayjs().year(), dayjs().month() + 1), dayjs().date())
+)
+const endYear = ref<number>(getArrayIndex(years(), dayjs().year()))
+const endMonth = ref<number>(getArrayIndex(months(), dayjs().month()))
+const endDay = ref<number>(getArrayIndex(days(dayjs().year(), dayjs().month() + 1), dayjs().date()))
+const currentValues = ref<number[]>([])
+
+// 初始化时，需要将实际的年、月、日转换为对应的年、月、日数组的下标进行回显
+watch(
+  () => [props.type, props.value],
+  (val: any) => {
+    if (val && val.length) {
+      if (val[0] === 'date') {
+        let arr = val[1] && val[1].length ? val[1].split('-') : []
+        if (arr && arr.length) {
+          startYear.value = getArrayIndex(years(), Number(arr[0]))
+          startMonth.value = getArrayIndex(months(), Number(arr[1]))
+          startDay.value = getArrayIndex(days(Number(arr[0]), Number(arr[1])), Number(arr[2]))
+        }
+        currentValues.value = [startYear.value, startMonth.value, startDay.value]
+      } else if (val[0] === 'daterange') {
+        let arr1 = val[1] && val[1].length ? val[1][0].split('-') : []
+        let arr2 = val[1] && val[1].length ? val[1][1].split('-') : []
+        if (arr1 && arr1.length) {
+          startYear.value = getArrayIndex(years(), Number(arr1[0]))
+          startMonth.value = getArrayIndex(months(), Number(arr1[1]))
+          startDay.value = getArrayIndex(days(Number(arr1[0]), Number(arr1[1])), Number(arr1[2]))
+        }
+        if (arr2 && arr2.length) {
+          endYear.value = getArrayIndex(years(), Number(arr2[0]))
+          endMonth.value = getArrayIndex(months(), Number(arr2[1]))
+          endDay.value = getArrayIndex(days(Number(arr2[0]), Number(arr2[1])), Number(arr2[2]))
+        }
+        currentValues.value = [
+          startYear.value,
+          startMonth.value,
+          startDay.value,
+          endYear.value,
+          endMonth.value,
+          endDay.value
+        ]
+      } else {
+        let arr = val[1] && val[1].length ? val[1].split('-') : []
+        if (arr && arr.length) {
+          startYear.value = getArrayIndex(years(), Number(arr[0]))
+          startMonth.value = getArrayIndex(months(), Number(arr[1]))
+          startDay.value = getArrayIndex(days(Number(arr[0]), Number(arr[1])), Number(arr[2]))
+        }
+        currentValues.value = [startYear.value, startMonth.value, startDay.value]
+      }
+    }
+  },
+  {
+    immediate: true,
+    deep: true
+  }
+)
+
 const change = (e: any) => {
   const val = e.detail.value
   if (props.type === 'date') {
-    startYear.value = years()[val[0]]
-    startMonth.value = months()[val[1]]
-    startDay.value = days(startYear.value, startMonth.value)[val[2]]
+    startYear.value = Number(val[0])
+    startMonth.value = Number(val[1])
+    startDay.value = Number(val[2])
   } else if (props.type === 'daterange') {
-    startYear.value = years()[val[0]]
-    startMonth.value = months()[val[1]]
-    startDay.value = days(startYear.value, startMonth.value)[val[2]]
-    endYear.value = years()[val[3]]
-    endMonth.value = months()[val[4]]
-    endDay.value = days(endYear.value, endMonth.value)[val[5]]
+    startYear.value = Number(val[0])
+    startMonth.value = Number(val[1])
+    startDay.value = Number(val[2])
+    endYear.value = Number(val[3])
+    endMonth.value = Number(val[4])
+    endDay.value = Number(val[5])
   } else {
-    startYear.value = years()[val[0]]
-    startMonth.value = months()[val[1]]
-    startDay.value = days(startYear.value, startMonth.value)[val[2]]
+    startYear.value = Number(val[0])
+    startMonth.value = Number(val[1])
+    startDay.value = Number(val[2])
   }
 }
 
@@ -222,17 +249,29 @@ const fmtNum = (val: number) => {
 const confirm = () => {
   visible.value = false
   if (props.type === 'date') {
-    emit('confirm', `${startYear.value}-${fmtNum(startMonth.value)}-${fmtNum(startDay.value)}`)
+    const startYears = years()[startYear.value]
+    const startMonths = months()[startMonth.value]
+    const startDays = days(startYears, startMonths)[startDay.value]
+    emit('confirm', `${startYears}-${fmtNum(startMonths)}-${fmtNum(startDays)}`)
   } else if (props.type === 'daterange') {
-    let startDate = `${startYear.value}-${fmtNum(startMonth.value)}-${fmtNum(startDay.value)}`
-    let endDate = `${endYear.value}-${fmtNum(endMonth.value)}-${fmtNum(endDay.value)}`
+    const startYears = years()[startYear.value]
+    const startMonths = months()[startMonth.value]
+    const startDays = days(startYears, startMonths)[startDay.value]
+    const endYears = years()[endYear.value]
+    const endMonths = months()[endMonth.value]
+    const endDays = days(endYears, endMonths)[endDay.value]
+    let startDate = `${startYears}-${fmtNum(startMonths)}-${fmtNum(startDays)}`
+    let endDate = `${endYears}-${fmtNum(endMonths)}-${fmtNum(endDays)}`
     if (dayjs(endDate).valueOf() < dayjs(startDate).valueOf()) {
       showToast('结束日期不能晚于开始日期')
       return
     }
     emit('confirm', [startDate, endDate])
   } else {
-    emit('confirm', `${startYear.value}-${fmtNum(startMonth.value)}-${fmtNum(startDay.value)}`)
+    const startYears = years()[startYear.value]
+    const startMonths = months()[startMonth.value]
+    const startDays = days(startYears, startMonths)[startDay.value]
+    emit('confirm', `${startYears}-${fmtNum(startMonths)}-${fmtNum(startDays)}`)
   }
 }
 </script>
