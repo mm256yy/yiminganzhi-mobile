69df6cc74a0921c7c4b48e5ef6aa189419d62ccc
2497976487@qq.com <2497976487@qq.com>
2024-03-28 18:22:19 +0800

    feat: 问题修复

--- src/pages/land/components/navigation/CompensateCard.vue
+++ src/pages/land/components/navigation/CompensateCard.vue
@@ -101,7 +101,7 @@
           <view class="td td-3">{{ formatStr(item.number) }}</view>
           <view class="td td-3">{{ formatStr(item.price) }}</view>
           <view class="td td-3">
-            <view v-if="item.isUpdate == '0'">{{ item.totalPrice }}</view>
+            <view v-if="item.isUpdate == '0'&&item.isSum == '0'">{{ item.totalPrice }}</view>
            <view v-else-if="item.isUpdate === '1' && item.isSum === '0'">{{
               computedTotalPrice(item)
             }}</view>
